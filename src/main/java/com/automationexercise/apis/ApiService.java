faac27de2ae0d5d17dad01c13c72ba18126a1a04
Ahmed Ashraf <69063398+Ashraaf7@users.noreply.github.com>
2024-03-28 04:05:33 +0200

    Update ApiService.java

--- src/main/java/com/automationexercise/apis/ApiService.java
+++ src/main/java/com/automationexercise/apis/ApiService.java
@@ -6,25 +6,6 @@ import static io.restassured.RestAssured.given;
 
 
 public class ApiService {
-//private static final  String baseURI = "http://automationexercise.com";
-  /*  public static void registerTestCase(String name, String email,String password,String firstname,String lastname,
-                                        String company,String address1,String country,String zipcode,String state,String city,String mobile_number ) {
-        HttpClient client = HttpClient.newHttpClient();
-        HttpRequest request = HttpRequest.newBuilder()
-                .uri(URI.create("https://automationexercise.com/api/createAccount"))
-                .header("Content-Type", "application/json")
-                .POST(HttpRequest.BodyPublishers.ofString("{\"name\": \"" + name + "\", \"email\": \"" +email+ "\",\"password\":\""+password+"\" , \"firstname\":\"" + firstname + "\", \"lastname\":\""
-                        + lastname + "\", \"company\":\"" +company+ "\", \"address1\": \"" + address1 + "\"" +
-                        ", \"country\":\""+ country+"\",\"zipcode\": \"" + zipcode +"\",\"state\":\"" +state + "\"," +
-                        " \"city\":\"" + city +"\",\"mobile_number\":\""+mobile_number+"\"}")).build();
-
-        try {
-            HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
-            System.out.println("Registration Status: " + response.statusCode());
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }*/
     public void registrationApi(String name, String email,String password,String firstname,String lastname,
                                 String company,String address1,String country,String zipcode,String state,String city,String mobile_number) {
          RestAssured.baseURI = "http://automationexercise.com";
