2cfd26a424700f1adb3e9bf2bdac5d13aa1d391f
ahmed__mohamed <ahmedmohamed660000@gmail.com>
2024-03-30 14:57:46 +0200

    Handle ADS

--- src/main/java/com/automationexercise/Utilities/Utility.java
+++ src/main/java/com/automationexercise/Utilities/Utility.java
@@ -1,5 +1,6 @@
 package com.automationexercise.Utilities;
 
+import com.automationexercise.Pages.P_AdPage;
 import org.openqa.selenium.*;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
